4663a648901c647c65243a4969735a8e11e51eff
Ali Rafieefar <aliraf62@gmail.com>
2023-09-14 17:40:27 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,22 @@
+let foundBy = ""
+radio.setGroup(1)
+
+// When someone finds M1 and presses its button
 input.onButtonPressed(Button.A, function () {
     if (foundBy == "M2") {
         radio.sendValue("TeamAScore", 1)
+        basic.showString("M2")
+        music.playMelody("C D E F", 120)
     } else if (foundBy == "M3") {
         radio.sendValue("TeamBScore", 1)
+        basic.showString("M3")
+        music.playMelody("G A B C5", 120)
     }
     foundBy = ""
-    // Reset the state for the next discovery
+    // Signal to reset for the next round
     radio.sendString("Reset")
 })
+
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "M2Found") {
         foundBy = "M2"
@@ -17,18 +26,15 @@ radio.onReceivedString(function (receivedString) {
         radio.sendString("ConfirmM3")
     }
 })
-let foundBy = ""
-radio.setGroup(1)
+
+// Broadcasts the compass heading regularly
 basic.forever(function () {
     radio.sendValue("M1Bearing", input.compassHeading())
     basic.pause(500)
 })
 
-basic.forever(function () {
-    if (foundBy == "M2" || foundBy == "M3") {
-        basic.showIcon(IconNames.Yes)
-    } else {
-        basic.clearScreen()
-    }
-    basic.pause(500)
-})
+// Calibration phase: M1 broadcasts a calibration signal
+for (let index = 0; index < 5; index++) {
+    radio.sendString("CalibrationSignal")
+    basic.pause(100)
+}
